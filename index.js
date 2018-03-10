ecbf6f0b607504e0d4488908393750fda04628f8
Andrew <andrew.gcorley@gmail.com>
2018-03-09 22:49:40 -0700

    dasdad

--- index.js
+++ index.js
@@ -159,12 +159,12 @@ http.listen(port, function(){
 });
 
 
-Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i--) {
-        if (this[i] === obj) {
-            return true;
-        }
-    }
-    return false;
-}
+// Array.prototype.contains = function(obj) {
+//     var i = this.length;
+//     while (i--) {
+//         if (this[i] === obj) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
